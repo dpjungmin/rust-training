54c925ebf0d4ea36e190a268c4512a046ab57085
David Park <dpjungmin@gmail.com>
2021-04-19 12:22:17 +0900

    update vec

--- vec/src/lib.rs
+++ vec/src/lib.rs
@@ -1,3 +1,4 @@
+use std::alloc::{self, Layout};
 use std::ptr::NonNull;
 
 pub struct MyVec<T> {
@@ -6,18 +7,55 @@ pub struct MyVec<T> {
     capacity: usize,
 }
 
+impl<T> MyVec<T> {
+    pub fn new() -> Self {
+        Self {
+            ptr: NonNull::dangling(),
+            len: 0,
+            capacity: 0,
+        }
+    }
+
+    pub fn push(&mut self, item: T) {
+        assert_ne!(std::mem::size_of::<T>(), 0, "No zero sized types");
+
+        if self.capacity == 0 {
+            let layout = Layout::array::<T>(4).expect("Failed to allocate memory");
+            // SAFETY: the layout is hard coded to be 4 * size_of(T) and size_of(T) > 0
+            let ptr = unsafe { alloc::alloc(layout) } as *mut T;
+            let ptr = NonNull::new(ptr).expect("Failed to allocate memory");
+            // SAFETY: ptr is non-null and we have enough space to write one item
+            unsafe { ptr.as_ptr().write(item) };
+            self.ptr = ptr;
+            self.len = 1;
+            self.capacity = 4;
+        } else {
+            todo!()
+        }
+    }
+
+    pub fn capacity(&self) -> usize {
+        self.capacity
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn it_works() {
-        let mut vec = Vec::new();
+        let mut vec = MyVec::<usize>::new();
         vec.push(1usize);
-        vec.push(2);
-        vec.push(3);
-        vec.push(4);
-        vec.push(5);
+        // vec.push(2);
+        // vec.push(3);
+        // vec.push(4);
+        // vec.push(5);
 
-        assert_eq!(vec.capacity(), 8);
-        assert_eq!(vec.len(), 5);
+        assert_eq!(vec.len(), 1);
+        assert_eq!(vec.capacity(), 4);
     }
 }
